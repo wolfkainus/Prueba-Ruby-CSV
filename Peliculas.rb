b4c113bdd5dc4f04cdb1510a82e4b496514c36bb
Alfredo Lobos <alfredo.lobos.g@gmail.com>
2015-10-05 01:39:14 -0300

    metod to_s for genre, dvd_title, year, rating

--- Peliculas.rb
+++ Peliculas.rb
@@ -5,4 +5,13 @@ class Movie
   def initialize(data = {})
     data.each { |name, value| instance_variable_set("@#{name}", value) }
   end
+#-----------------------------------------------------------------------------------------
+
+#3. Debe haber un método to_s que devuelva el 
+#Nombre, fecha de estreno, genero y clasificación del a pelicula.
+#-----------------------------------------------------------------------------------------
+  def to_s
+    "[#{@genre}] #{@dvd_title} (#{@year}) #{@rating}"
+  end
+#-----------------------------------------------------------------------------------------
 end
