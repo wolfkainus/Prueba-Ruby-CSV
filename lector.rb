91744643b4e090dac5299e65856c38e6e7c03a6c
Alfredo Lobos <alfredo.lobos.g@gmail.com>
2015-10-05 01:54:56 -0300

    Delete with RUBY: Status, Sound, Versions, Aspect, UPC, DVD_RealeaseDate, Id, Timestamp

--- lector.rb
+++ lector.rb
@@ -2,6 +2,14 @@ require 'csv'
 
 def lector
   csv = CSV.read('dvd.csv', headers: true, header_converters: :symbol, converters: :all)
+
+#6. Deben eliminar (mediante ruby al leer el csv) las columnas:
+#Status, Sound, Versions, Aspect, UPC, DVD_RealeaseDate, Id, Timestamp
+#-----------------------------------------------------------------------------------------
+useless = %i(status sound versions aspect upc dvd_releasedate id timestamp)
+useless.each { |e| csv.delete(e) }
+#-----------------------------------------------------------------------------------------
+
   return csv.map(&:to_hash)
 end
 
